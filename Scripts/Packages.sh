8dfd705297247a2e63392bed93b3dfd6105fefe9
davidtall <269700927@qq.com>
2025-12-02 09:39:38 +0800

    编译 lucky nikki

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -72,6 +72,7 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 
 UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "master"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
+UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
