851e6634dd6f35c953e8e1753d27573956caae6d
david <269700927@qq.com>
2026-03-05 12:44:42 +0800

    调试

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -135,4 +135,5 @@ rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makef
 cat luci-app-daed/daed/Makefile
 #修复libubox报错
 sed -i '/include $(INCLUDE_DIR)\/cmake.mk/a PKG_BUILD_FLAGS:=no-werror' ../package/libs/libubox/Makefile
+sed -i 's|TARGET_CFLAGS += -I$(STAGING_DIR)/usr/include|& -Wno-error=format-nonliteral -Wno-format-nonliteral|' ../package/libs/libubox/Makefile
 cat ../package/libs/libubox/Makefile
